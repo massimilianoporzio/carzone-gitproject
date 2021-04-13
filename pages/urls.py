5a81350888111251aad407237f00de96afa8647a
Massimiliano <massimiliano.porzio@gmail.com>
2021-04-13 10:47:05 +0200

    navigazione pagine con template e base.html

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import (
+    HomePageView,AboutPageView,
+    ServicesPageView,ContactPageView
+)
+
+app_name='pages'
 
 urlpatterns = [
-    path('',HomePageView.as_view(),name='home')
+    path('',HomePageView.as_view(),name='home'),
+    path('about',AboutPageView.as_view(),name='about'),
+    path('services',ServicesPageView.as_view(),name='services'),
+    path('contact',ContactPageView.as_view(),name='contact'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html '
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+class ServicesPageView(TemplateView):
+    template_name = 'pages/services.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
